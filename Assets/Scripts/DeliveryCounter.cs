fb3458e8371217181282d567a41d6ad2ae0d1220
ilmaralamsyah <ilmaralamsyah@gmail.com>
2024-06-08 10:37:19 +0700

    DeliveryCounter_Shader_YTB

---
+++ Assets/Scripts/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)){
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
